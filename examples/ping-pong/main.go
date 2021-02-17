4be5456f4b9d20e0cb7b375f4e6f1da01e826cbb
郭泽欣 <guozexin.guo@bytedance.com>
2021-02-17 21:50:24 +0800

    use single server

--- examples/ping-pong/main.go
+++ examples/ping-pong/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -17,11 +19,22 @@ import (
 
 //nolint
 var (
-	isControlling                 bool
-	iceAgent                      *ice.Agent
-	remoteAuthChannel             chan string
-	localHTTPPort, remoteHTTPPort int
+	isControlling bool
+	isServer      bool
+
+	iceAgent              *ice.Agent
+	remoteAuthChannel     chan string
+	localCandidateChannel chan string
+
+	remoteHTTPHost string
+	remoteHTTPPort int
 )
+
+type CandidateInfo struct {
+	Ufrag     string `json:"ufrag,omitempty"`
+	Pwd       string `json:"pwd"`
+	Candidate string `json:"candidate"`
+}
 
 // HTTP Listener to get ICE Credentials from remote Peer
 func remoteAuth(w http.ResponseWriter, r *http.Request) {
@@ -31,10 +44,22 @@ func remoteAuth(w http.ResponseWriter, r *http.Request) {
 
 	remoteAuthChannel <- r.PostForm["ufrag"][0]
 	remoteAuthChannel <- r.PostForm["pwd"][0]
+	log.Printf("receive remote auth")
+
+	// Get the local auth details and send to remote peer
+	localUfrag, localPwd, err := iceAgent.GetLocalUserCredentials()
+	if err != nil {
+		panic(err)
+	}
+
+	c := &CandidateInfo{Ufrag: localUfrag, Pwd: localPwd}
+	json.NewEncoder(w).Encode(c)
 }
 
 // HTTP Listener to get ICE Candidate from remote Peer
 func remoteCandidate(w http.ResponseWriter, r *http.Request) {
+	log.Printf("receive remote candidate")
+
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
@@ -47,31 +72,184 @@ func remoteCandidate(w http.ResponseWriter, r *http.Request) {
 	if err := iceAgent.AddRemoteCandidate(c); err != nil {
 		panic(err)
 	}
+	localCandidate := <-localCandidateChannel
+	data := &CandidateInfo{Candidate: localCandidate}
+	json.NewEncoder(w).Encode(data)
 }
 
+// go run main.go -server
+//  go run main.go -controlling
 // export PION_LOG_TRACE=all
 func main() { //nolint
+	remoteAuthChannel = make(chan string, 3)
+
+	flag.BoolVar(&isControlling, "controlling", false, "is ICE Agent controlling")
+
+	flag.BoolVar(&isServer, "server", false, "is server")
+
+	flag.Parse()
+	remoteAuthChannel = make(chan string, 3)
+	localCandidateChannel = make(chan string)
+
+	remoteHTTPHost = "localhost:9001"
+	remoteHTTPPort = 9001
+
+	if isServer {
+		server()
+	} else {
+		client()
+	}
+}
+
+func client() { //nolint
 	var (
 		err  error
 		conn *ice.Conn
 	)
-	remoteAuthChannel = make(chan string, 3)
-
-	flag.BoolVar(&isControlling, "controlling", false, "is ICE Agent controlling")
-	flag.Parse()
+	http.DefaultClient.Timeout = 3 * time.Second
 
 	if isControlling {
-		localHTTPPort = 9000
-		remoteHTTPPort = 9001
+		fmt.Println("Local Agent is controlling")
 	} else {
-		localHTTPPort = 9001
-		remoteHTTPPort = 9000
+		fmt.Println("Local Agent is controlled")
 	}
+	fmt.Print("Press 'Enter' when both processes have started")
+	if _, err = bufio.NewReader(os.Stdin).ReadBytes('\n'); err != nil {
+		panic(err)
+	}
+
+	iceAgent, err = ice.NewAgent(&ice.AgentConfig{
+		NetworkTypes: []ice.NetworkType{ice.NetworkTypeUDP4},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// When we have gathered a new ICE Candidate send it to the remote peer
+	if err = iceAgent.OnCandidate(func(c ice.Candidate) {
+		if c == nil {
+			return
+		}
+
+		log.Printf("local candidate is : %s\n", c)
+		resp, err := http.PostForm(fmt.Sprintf("http://%s/remoteCandidate", remoteHTTPHost), //nolint
+			url.Values{
+				"candidate": {c.Marshal()},
+			})
+		if err != nil {
+			panic(err)
+		}
+
+		bytes, err := parseResp(resp)
+		if err != nil {
+			panic(err)
+		}
+		candidate := &CandidateInfo{}
+		err = json.Unmarshal(bytes, candidate)
+		if err != nil {
+			panic(err)
+		}
+
+		remoteCandidate, err := ice.UnmarshalCandidate(candidate.Candidate)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := iceAgent.AddRemoteCandidate(remoteCandidate); err != nil {
+			panic(err)
+		}
+	}); err != nil {
+		panic(err)
+	}
+
+	// When ICE Connection state has change print to stdout
+	if err = iceAgent.OnConnectionStateChange(func(c ice.ConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", c.String())
+	}); err != nil {
+		panic(err)
+	}
+
+	// Get the local auth details and send to remote peer
+	localUfrag, localPwd, err := iceAgent.GetLocalUserCredentials()
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.PostForm(fmt.Sprintf("http://%s/remoteAuth", remoteHTTPHost), //nolint
+		url.Values{
+			"ufrag": {localUfrag},
+			"pwd":   {localPwd},
+		})
+	if err != nil {
+		panic(err)
+	}
+	bytes, err := parseResp(resp)
+	if err != nil {
+		panic(err)
+	}
+	candidate := &CandidateInfo{}
+	err = json.Unmarshal(bytes, candidate)
+	if err != nil {
+		panic(err)
+	}
+
+	remoteUfrag := candidate.Ufrag
+	remotePwd := candidate.Pwd
+
+	// todo 收集自身的candidate，然后将自身candidate，发送给对方
+	if err = iceAgent.GatherCandidates(); err != nil {
+		panic(err)
+	}
+
+	// Start the ICE Agent. One side must be controlled, and the other must be controlling
+	if isControlling {
+		conn, err = iceAgent.Dial(context.TODO(), remoteUfrag, remotePwd)
+	} else {
+		conn, err = iceAgent.Accept(context.TODO(), remoteUfrag, remotePwd)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// Send messages in a loop to the remote peer
+	go func() {
+		for {
+			time.Sleep(time.Second * 3)
+
+			val, err := randutil.GenerateCryptoRandomString(15, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+			if err != nil {
+				panic(err)
+			}
+			if _, err = conn.Write([]byte(val)); err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("Sent: '%s'\n", val)
+		}
+	}()
+
+	// Receive messages in a loop from the remote peer
+	buf := make([]byte, 1500)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Received: '%s'\n", string(buf[:n]))
+	}
+}
+
+func server() { //nolint
+	var (
+		err  error
+		conn *ice.Conn
+	)
 
 	http.HandleFunc("/remoteAuth", remoteAuth)
 	http.HandleFunc("/remoteCandidate", remoteCandidate)
 	go func() {
-		if err = http.ListenAndServe(fmt.Sprintf(":%d", localHTTPPort), nil); err != nil {
+		if err = http.ListenAndServe(fmt.Sprintf(":%d", remoteHTTPPort), nil); err != nil {
 			panic(err)
 		}
 	}()
@@ -100,13 +278,7 @@ func main() { //nolint
 		}
 
 		log.Printf("local candidate is : %s\n", c)
-		_, err = http.PostForm(fmt.Sprintf("http://localhost:%d/remoteCandidate", remoteHTTPPort), //nolint
-			url.Values{
-				"candidate": {c.Marshal()},
-			})
-		if err != nil {
-			panic(err)
-		}
+		localCandidateChannel <- c.Marshal()
 	}); err != nil {
 		panic(err)
 	}
@@ -115,21 +287,6 @@ func main() { //nolint
 	if err = iceAgent.OnConnectionStateChange(func(c ice.ConnectionState) {
 		fmt.Printf("ICE Connection State has changed: %s\n", c.String())
 	}); err != nil {
-		panic(err)
-	}
-
-	// Get the local auth details and send to remote peer
-	localUfrag, localPwd, err := iceAgent.GetLocalUserCredentials()
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = http.PostForm(fmt.Sprintf("http://localhost:%d/remoteAuth", remoteHTTPPort), //nolint
-		url.Values{
-			"ufrag": {localUfrag},
-			"pwd":   {localPwd},
-		})
-	if err != nil {
 		panic(err)
 	}
 
@@ -178,4 +335,19 @@ func main() { //nolint
 
 		fmt.Printf("Received: '%s'\n", string(buf[:n]))
 	}
+}
+
+func parseResp(resp *http.Response) ([]byte, error) {
+	defer resp.Body.Close()
+
+	// check response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode > 399 { // error
+		return nil, err
+	}
+
+	return body, err
 }
